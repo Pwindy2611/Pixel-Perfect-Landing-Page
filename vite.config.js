0bb76fc668efe1bbeb32660f38de71399b2e7ac3
Phong Nguyen <hoangphongcuade@gmail.com>
2026-01-20 22:15:23 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import copy from "rollup-plugin-copy";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        copy({
+          targets: [
+            { src: "src/**/*", dest: "dist/src" },
+          ],
+          hook: "writeBundle",
+        }),
+      ],
+    },
+  },
+});
